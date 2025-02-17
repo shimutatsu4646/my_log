fb4b74cdfcaca563b76fffd6daf12f7c076fc7fd
shimutatsu4646 <3shimutatsu.4646@gmail.com>
2025-02-17 23:43:31 +0900

    ADD: スイングハイ・ロウのインジケーターを作成

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ cp ~/my_log/alpha.mq5 "${HOME}/Library/Application Support/net.metaquotes.wine.m
 
 # Includeフォルダ配下のファイルをデプロイ
 cp ~/my_log/Include/* "${HOME}/Library/Application Support/net.metaquotes.wine.metatrader5/drive_c/Program Files/MetaTrader 5/MQL5/Include/MyCode/"
+
+# Indicatorsフォルダ配下のファイルをデプロイ
+cp ~/my_log/Indicators/* "${HOME}/Library/Application Support/net.metaquotes.wine.metatrader5/drive_c/Program Files/MetaTrader 5/MQL5/Indicators/MyCode/"
